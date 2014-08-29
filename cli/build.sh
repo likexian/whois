e970ca38f1c1b5e6351ac8132d303c83e911e8d5
Li Kexian <i@likexian.com>
2014-08-29 16:27:02 +0800

    fixed build script

--- cli/build.sh
+++ cli/build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-go build main.go
-mv main whois
+go build -o whois main.go
+
+if [ ! -x ./whois ]; then
+    echo "build fail."
+    exit 1
+fi
 
 echo
 echo "build success."
 echo "you can use [./whois domain] to query whois now."
 echo
-echo "for example:"
-echo "./whois likexian.com"
+echo "for example: ./whois likexian.com"
 echo
 echo "press any key to test the above example."
 echo
