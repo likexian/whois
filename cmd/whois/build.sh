1469fb4cd5c5b599165418e5de5bc96f7cbd1f25
Li Kexian <i@likexian.com>
2019-11-05 23:23:20 +0800

    add trimpath arguments to go build

--- cmd/whois/build.sh
+++ cmd/whois/build.sh
@@ -2,7 +2,7 @@
 
 for i in 386 amd64; do
     echo "Building whois.linux-$i..."
-    GOOS=linux GOARCH=$i go build -v -ldflags '-w -s' -o whois
+    GOOS=linux GOARCH=$i go build -v -trimpath -ldflags '-w -s' -o whois
     tar zcf whois.linux-$i.tar.gz whois
     rm -rf whois
 done
