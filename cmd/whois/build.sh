ba7d17fa80d3352016515c891341170972ee642c
Li Kexian <i@likexian.com>
2020-05-11 22:58:34 +0800

    add build cmd whois for darwin

--- cmd/whois/build.sh
+++ cmd/whois/build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+arch=386
 for os in linux windows; do
-    for arch in 386 amd64; do
-        echo "Building whois-$os-$arch..."
-        GOOS=$os GOARCH=$arch go build -v -trimpath -ldflags '-w -s' -o whois
-        zip whois-$os-$arch.zip whois
-        rm -rf whois
-    done
+    echo "Building whois-$os-$arch..."
+    GOOS=$os GOARCH=$arch go build -trimpath -ldflags '-w -s' -o whois
+    zip whois-$os-$arch.zip whois
+    rm -rf whois
+done
+
+arch=amd64
+for os in linux windows darwin; do
+    echo "Building whois-$os-$arch..."
+    GOOS=$os GOARCH=$arch go build -trimpath -ldflags '-w -s' -o whois
+    zip whois-$os-$arch.zip whois
+    rm -rf whois
 done
