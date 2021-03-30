4ac71d1237407677d2fdba9798804617548e224a
SteveYi <jackcooku@steveyi.net>
2021-03-31 04:14:08 +0800

    Add a rule to check if IP Address is IPv4 Cidr

--- whois.go
+++ whois.go
@@ -137,8 +137,12 @@ func getExtension(domain string) string {
 	ext := domain
 
 	if net.ParseIP(domain) == nil {
-		domains := strings.Split(domain, ".")
-		ext = domains[len(domains)-1]
+		if net.ParseIP(strings.Split(domain, "/")[0]) != nil {
+			ext = strings.Split(domain, "/")[0]
+		} else {
+			domains := strings.Split(domain, ".")
+			ext = domains[len(domains)-1]
+		}
 	}
 
 	if strings.Contains(ext, "/") {
